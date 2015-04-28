2382366c5354c83ee0a522fd29f47f69a48fca37
Yangzi <yangzihe12@gmail.com>
2015-04-28 15:24:04 -0700

    Fixed migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421194433) do
+ActiveRecord::Schema.define(version: 20150418011410) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "species"
