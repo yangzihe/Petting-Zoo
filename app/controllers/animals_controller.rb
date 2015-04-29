22d359585aa35b8e5bd29669c2394f95ed75c509
Yangzi <yangzihe12@gmail.com>
2015-04-29 00:32:09 -0700

    Added missing end

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,41 +1,41 @@
 class AnimalsController < ApplicationController
 
-def new 
-	@new_animal = Animal.new 
-end 
-
-def create 
-	@new_animal = Animal.new
-	@new_animal.species = params[:animal]['species']
-	@new_animal.energy = 10
-	@new_animal.pen_id = current_pen.id
-	if @new_animal.save
-			redirect_to pen_path(id: current_pen.id)
-		else 
-			flash[:error] = @new_animal.errors.full_messages.to_sentence
-			redirect_to animal_new_path
+	def new
+		@new_animal = Animal.new
 	end
-end
 
-def feed 
-	@animal1 = Animal.find(params[:id])
-	if @animal1.health < 10 
-		@animal1.update_attribute(:health, @new_animal.health + 3)
-		if @animal1.save
-			redirect_to pen_path(id: current_pen.id)
+	def create
+		@new_animal = Animal.new
+		@new_animal.species = params[:animal]['species']
+		@new_animal.energy = 10
+		@new_animal.pen_id = current_pen.id
+		if @new_animal.save
+				redirect_to pen_path(id: current_pen.id)
+			else
+				flash[:error] = @new_animal.errors.full_messages.to_sentence
+				redirect_to animal_new_path
+		end
 	end
-end 
 
-# pet in visitor controller? decreases animal energy 
+	def feed
+		@animal1 = Animal.find(params[:id])
+		if @animal1.health < 10
+			@animal1.update_attribute(:health, @new_animal.health + 3)
+			if @animal1.save
+				redirect_to pen_path(id: current_pen.id)
+			end
+		end
+	end
 
-def delete
-	@animal1 = Animal.find(params[:id])
-    @animal1.destroy
-    redirect_to pen_path(id: current_pen.id)
-end
+# pet in visitor controller? decreases animal energy
 
-def index 
-	@animals = Animal.all
-end 
+	def delete
+		@animal1 = Animal.find(params[:id])
+	    @animal1.destroy
+	    redirect_to pen_path(id: current_pen.id)
+	end
 
+	def index
+		@animals = Animal.all
+	end
 end
